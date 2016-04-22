855c5a62ac09cde33e29bc4573ac44eaa848bc9d
Casey Foster <c@sey.me>
2016-04-21 22:44:05 -0500

    update and clone do not need to be publicy exposed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import clone from './clone';
 import Router from './router';
 import Store from './store';
 import SyncPromise from './sync-promise';
 import toKey from './to-key';
-import update from './update';
 
-export {clone, Router, Store, SyncPromise, toKey, update};
+export {Router, Store, SyncPromise, toKey};
