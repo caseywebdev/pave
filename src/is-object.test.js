108d7b30670aeb4757b59572b614fb3ffb5fa4b0
Casey Foster <c@sey.me>
2021-11-04 14:29:16 -0500

    Add basic assertions

---
+++ src/is-object.test.js
@@ -0,0 +1,27 @@
+import { strict as assert } from 'assert';
+
+import isObject from './is-object.js';
+
+export default {
+  withKeys: () => assert.deepEqual(isObject({ a: 1 }), true),
+
+  'without keys': () => assert.deepEqual(isObject({}), true),
+
+  null: () => assert.deepEqual(isObject(null), false),
+
+  undefined: () => assert.deepEqual(isObject(undefined), false),
+
+  array: () => assert.deepEqual(isObject([]), true),
+
+  boolean: () => assert.deepEqual(isObject(true), false),
+
+  number: () => assert.deepEqual(isObject(0), false),
+
+  string: () => assert.deepEqual(isObject('str'), false),
+
+  function: () =>
+    assert.deepEqual(
+      isObject(() => {}),
+      false
+    )
+};
