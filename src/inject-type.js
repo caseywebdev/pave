229928a021b1b53016c18570a8009ebf1b56fd09
Casey Foster <c@sey.me>
2022-03-08 17:00:56 -0600

    Don't inject _type into non-objects, 0.11.4

--- src/inject-type.js
+++ src/inject-type.js
@@ -1,19 +1,15 @@
-import isArray from './is-array.js';
 import isObject from './is-object.js';
 
 const injectType = query => {
+  if (!isObject(query)) return query;
+
   query = { _type: {}, ...query };
   for (const key in query) {
-    if (
-      key !== '_args' &&
-      key !== '_field' &&
-      key !== '_type' &&
-      isObject(query) &&
-      !isArray(query)
-    ) {
-      query[key] = injectType(query[key]);
-    }
+    if (key === '_args' || key === '_field' || key === '_type') continue;
+
+    query[key] = injectType(query[key]);
   }
+
   return query;
 };
 
