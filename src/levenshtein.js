7264bdaddb73e4057bd42409c390504b026aa9ed
Casey Foster <c@sey.me>
2022-10-19 12:00:27 -0500

    Improve errors, 0.11.17

---
+++ src/levenshtein.js
@@ -0,0 +1,20 @@
+export default (a, b) => {
+  let [v0, v1] = [new Array(b.length + 1), new Array(b.length + 1)];
+
+  for (let i = 0; i <= b.length; ++i) v0[i] = i;
+
+  for (let i = 0; i < a.length; ++i) {
+    v1[0] = i + 1;
+    const aV = a[i];
+    for (let j = 0; j < b.length; ++j) {
+      v1[j + 1] = Math.min(
+        v0[j] + (aV === b[j] ? 0 : 1),
+        v0[j + 1] + 1,
+        v1[j] + 1
+      );
+    }
+    [v0, v1] = [v1, v0];
+  }
+
+  return v0[b.length];
+};
--- src/throw-pave-error.js
+++ src/throw-pave-error.js
@@ -1,3 +1,4 @@
+import levenshtein from './levenshtein.js';
 import PaveError from './pave-error.js';
 
 const { Intl } = globalThis;
@@ -7,68 +8,70 @@ const formatOr = values =>
     type: 'disjunction'
   }).format(values.map(value => `"${value}"`));
 
+const getSuggestion = (provided, expected) => {
+  const suggestion = new Intl.ListFormat(undefined, {
+    type: 'disjunction'
+  }).format(
+    expected
+      .map(value => ({ distance: levenshtein(provided, value), value }))
+      .sort((a, b) => (a.distance < b.distance ? -1 : 1))
+      .flatMap(({ distance, value }) => (distance <= 5 ? `"${value}"` : []))
+  );
+  return suggestion && `. Did you mean ${suggestion}?`;
+};
+
 const formatPath = path =>
-  path.length === 0 ? 'the query root' : `"${path.join('.')}"`;
+  path.length === 0 ? 'the query root' : `"${path.join('"."')}"`;
 
 const messages = {
   expectedArray: ({ path, value }) =>
-    `An array was expected at ${formatPath(path)} but ${JSON.stringify(
-      value
-    )} was provided`,
-
-  expectedArrayMaxLength: ({ type: { minLength }, path, value: { length } }) =>
-    `An array of at most length ${minLength} was expected at ${formatPath(
+    `The value ${JSON.stringify(value)} at ${formatPath(
       path
-    )} but an array of length ${length} was provided`,
+    )} must be an array`,
+
+  expectedArrayMaxLength: ({ type: { maxLength }, path, value: { length } }) =>
+    `The array with length ${length} at ${formatPath(
+      path
+    )} must be at most length ${maxLength}`,
 
   expectedArrayMinLength: ({ path, type: { minLength }, value: { length } }) =>
-    `An array of at least length ${minLength} was expected at ${formatPath(
+    `The array with length ${length} at ${formatPath(
       path
-    )} but an array of length ${length} was provided`,
+    )} must be at least length ${minLength}`,
 
-  expectedNonNull: ({ path, value }) =>
-    `A non-null value was expected at ${formatPath(path)} but ${JSON.stringify(
-      value
-    )} was provided`,
+  expectedNonNull: ({ path }) =>
+    `The value at ${formatPath(path)} cannot be null`,
 
   expectedOneOfType: ({ type: { oneOf }, path, value }) =>
-    `A value matching one of type ${formatOr(
-      Object.keys(oneOf)
-    )} was expected at ${formatPath(path)} but ${JSON.stringify(
-      value
-    )} was provided`,
+    `The value ${JSON.stringify(value)} at ${formatPath(
+      path
+    )} does not resolve to ${formatOr(Object.keys(oneOf))}`,
 
   expectedOneOfTypeField: ({ type: { oneOf }, path, field }) =>
-    `A field matching one of ${formatOr(
+    `The field ${JSON.stringify(field)} at ${formatPath(
+      path
+    )} does not exist${getSuggestion(
+      field,
       Object.keys(oneOf).map(key => `_on_${key}`)
-    )} was expected at ${formatPath(path)} but ${JSON.stringify(
-      field
-    )} was provided`,
+    )}`,
 
-  expectedRequired: ({ path }) =>
-    `A value was expected at ${formatPath(path)} but nothing was provided`,
+  expectedRequired: ({ path }) => `A value at ${formatPath(path)} is required`,
 
   invalidQuery: ({ path, query }) =>
-    `Expected a query object at ${formatPath(path)} but ${JSON.stringify(
-      query
-    )} was provided`,
+    `The query object ${JSON.stringify(query)} at ${formatPath(
+      path
+    )} but ${JSON.stringify(query)} is invalid`,
 
   unexpectedField: ({ field, path }) =>
-    `No fields were expected at ${formatPath(
-      path
-    )} but "${field}" was provided`,
+    `The field ${JSON.stringify(field)} at ${formatPath(path)} is not expected`,
 
   unknownField: ({ type: { fields }, path, field }) =>
-    `A field matching one of ${formatOr(
-      Object.keys(fields)
-    )} was expected at ${formatPath(path)} but ${JSON.stringify(
-      field
-    )} was provided`,
+    `The field ${JSON.stringify(field)} at ${formatPath(
+      path
+    )} does not exist${getSuggestion(field, Object.keys(fields))}`,
 
   unknownType: ({ path, type }) =>
-    `The type ${JSON.stringify(type)} at ${formatPath(
-      path
-    )} could not be found in the schema`
+    `The type ${JSON.stringify(type)} at ${formatPath(path)} does not exist`
 };
 
 export default (code, context) => {
